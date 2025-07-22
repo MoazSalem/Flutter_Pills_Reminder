7bcb0070dacc1a4a8d4960774d7515487fbded11
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-07-22 19:36:00 +0300

    Custom text form field for medication screen

---
+++ lib/features/medications/presentation/widgets/custom_text_formfield.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:pills_reminder/core/styles/sizes.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String labelText;
+  final String? Function(String?)? validator;
+  const CustomTextFormField({
+    super.key,
+    this.controller,
+    required this.labelText,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+    return TextFormField(
+      validator: validator,
+      controller: controller,
+      onTapOutside: (_) => FocusManager.instance.primaryFocus?.unfocus(),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: theme.surfaceContainer,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.all(
+            Radius.circular(AppSizes.roundedRadius),
+          ),
+        ),
+        labelText: labelText,
+        contentPadding: EdgeInsets.symmetric(
+          vertical: AppSizes.largePadding,
+          horizontal: AppSizes.largePadding,
+        ),
+        alignLabelWithHint: true,
+      ),
+    );
+  }
+}
