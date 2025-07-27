9817bc924efdf91da8c677f8a752c3f60b412b69
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-07-27 17:49:05 +0300

    Medications repo

---
+++ lib/features/medications/data/repositories/medications_repo_impl.dart
@@ -0,0 +1,34 @@
+import 'package:pills_reminder/features/medications/data/models/medication_model.dart';
+import 'package:pills_reminder/features/medications/domain/datasources/medication_local_data_source.dart';
+import 'package:pills_reminder/features/medications/domain/entities/medication.dart';
+import 'package:pills_reminder/features/medications/domain/repositories/medications_repo.dart';
+
+class MedicationsRepoImpl implements MedicationsRepo {
+  final MedicationLocalDataSource localDataSource;
+  MedicationsRepoImpl({required this.localDataSource});
+
+  @override
+  Future<List<Medication>> getAllMedications() {
+    return localDataSource.getAll().then(
+      (medications) => medications.map((med) => med.toEntity()).toList(),
+    );
+  }
+
+  @override
+  Future<void> addMedication(MedicationModel med) {
+    localDataSource.add(med);
+    return Future.value();
+  }
+
+  @override
+  Future<void> deleteMedication(int id) {
+    localDataSource.delete(id.toString());
+    return Future.value();
+  }
+
+  @override
+  Future<void> updateMedication(MedicationModel med) {
+    localDataSource.update(med);
+    return Future.value();
+  }
+}
