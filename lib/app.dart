3c5ff3bb14fc68ca616046cef17fd60ccaf9c601
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-07-27 13:43:54 +0300

    Add GetX and Hive ce

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:pills_reminder/core/styles/strings.dart';
 import 'package:pills_reminder/core/styles/theme.dart';
 import 'package:pills_reminder/features/main_screen/presentation/screens/main_screen/main_screen.dart';
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: AppStrings.appName,
       theme: AppTheme.lightTheme,
