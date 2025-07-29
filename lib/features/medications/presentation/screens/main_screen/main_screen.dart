b173dd689ff17833665b97dd4b7e7cc3f9d4c6b5
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-07-29 15:40:13 +0300

    Change System Navigation Bar Color

--- lib/features/medications/presentation/screens/main_screen/main_screen.dart
+++ lib/features/medications/presentation/screens/main_screen/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:pills_reminder/core/styles/sizes.dart';
 import 'package:pills_reminder/core/styles/strings.dart';
@@ -13,27 +14,35 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<MedicationController>();
-    final theme = Theme.of(context).colorScheme;
+    final theme = Theme.of(context);
 
     return Obx(() {
       if (!controller.isReady.value) {
         return CircularProgressIndicator();
       }
-      return Scaffold(
-        backgroundColor: theme.surface,
-        appBar: AppBar(
-          title: const Text(AppStrings.appName, style: AppStyles.title),
-          centerTitle: true,
-          toolbarHeight: AppSizes.appBarHeight,
-          backgroundColor: theme.surfaceContainer,
+      return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          systemNavigationBarColor: theme.colorScheme.surface,
+          systemNavigationBarIconBrightness: theme.brightness == Brightness.dark
+              ? Brightness.light
+              : Brightness.dark,
         ),
-        body: Obx(() {
-          final medications = controller.medications;
-          return medications.isEmpty
-              ? const Center(child: Text(AppStrings.noPills))
-              : MedicationList(medicationList: medications);
-        }),
-        floatingActionButton: Fab(theme: theme),
+        child: Scaffold(
+          backgroundColor: theme.colorScheme.surface,
+          appBar: AppBar(
+            title: const Text(AppStrings.appName, style: AppStyles.title),
+            centerTitle: true,
+            toolbarHeight: AppSizes.appBarHeight,
+            backgroundColor: theme.colorScheme.surfaceContainer,
+          ),
+          body: Obx(() {
+            final medications = controller.medications;
+            return medications.isEmpty
+                ? const Center(child: Text(AppStrings.noPills))
+                : MedicationList(medicationList: medications);
+          }),
+          floatingActionButton: Fab(theme: theme.colorScheme),
+        ),
       );
     });
   }
