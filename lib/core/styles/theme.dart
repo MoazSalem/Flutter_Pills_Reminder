160fc05b50f17b89c6add72c3988a98802bd58ab
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-07-31 14:45:10 +0300

    Change apps theme

--- lib/core/styles/theme.dart
+++ lib/core/styles/theme.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final lightTheme = ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
   );
 
   static final darkTheme = ThemeData(
     brightness: Brightness.dark,
     colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.greenAccent,
+      seedColor: Colors.orangeAccent,
       brightness: Brightness.dark,
     ),
   );
