df703cf0e1eaac8b12b829b58d525d8e930c5003
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-08-12 14:34:34 +0300

    Clean main activity

--- android/app/src/main/kotlin/com/moazsalem/pills_reminder/MainActivity.kt
+++ android/app/src/main/kotlin/com/moazsalem/pills_reminder/MainActivity.kt
@@ -1,29 +1,6 @@
 package com.moazsalem.pills_reminder
 
-import android.os.Build
-import android.provider.Settings
-import android.app.AlarmManager
-import android.content.Context
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
-    private val CHANNEL = "exact_alarm_channel"
 
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-                call, result ->
-            if (call.method == "checkExactAlarmPermission") {
-                val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    result.success(alarmManager.canScheduleExactAlarms())
-                } else {
-                    result.success(true)
-                }
-            }
-        }
-    }
-}
+class MainActivity : FlutterActivity()
