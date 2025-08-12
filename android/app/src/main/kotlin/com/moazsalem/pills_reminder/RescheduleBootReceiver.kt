1ea48c2a309f30cd1a64b7b1bed4b0e11dfcfc62
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-08-12 14:39:10 +0300

    Native function to reschedule All Notifications on restart

---
+++ android/app/src/main/kotlin/com/moazsalem/pills_reminder/RescheduleBootReceiver.kt
@@ -0,0 +1,48 @@
+package com.moazsalem.pills_reminder
+
+import android.util.Log
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.embedding.engine.loader.FlutterLoader
+import io.flutter.plugin.common.MethodChannel
+
+class RescheduleBootReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (context == null || intent?.action != Intent.ACTION_BOOT_COMPLETED) return
+
+            Log.i("RescheduleBootReceiver", "Boot completed — starting notification reschedule")
+
+            // Initialize Flutter engine loader
+            val loader = FlutterLoader()
+            loader.startInitialization(context)
+            loader.ensureInitializationComplete(context, null)
+
+            // Create a new FlutterEngine
+            val engine = FlutterEngine(context)
+
+            // Execute your background Dart entrypoint
+        engine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint(
+                loader.findAppBundlePath(),
+                "rescheduleAllNotifications" // Dart function name
+            )
+        )
+
+            // Create MethodChannel for Dart -> Kotlin communication to kill this engine instance
+            MethodChannel(
+                engine.dartExecutor.binaryMessenger,
+                "boot_reschedule_channel"
+            ).setMethodCallHandler { call, _ ->
+                if (call.method == "rescheduleComplete") {
+                    Log.i("RescheduleBootReceiver", "Dart finished rescheduling. Destroying engine.")
+                    engine.destroy()
+                }
+            }
+
+        }
+}
+
